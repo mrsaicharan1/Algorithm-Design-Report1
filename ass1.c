520c16e0c461c4c328f3ea3f6a3edcde5b4124b8
Saicharan Reddy <saicharan.reddy1@gmail.com>
2018-01-29 10:25:55 +0000

    Added sashis code

--- ass1.c
+++ ass1.c
@@ -1,80 +1,92 @@
-#include<stdio.h>
-#include<math.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int DecimalPlaces(float num){
-    int count=0;
-    while(round(num)!=0)
-    {
-        count++;
-        num=num/10;
-      
-    }
-    return count;
-}
-int NumberOfDigits(int num)
-{
-    int count=0;
-    while(round(num)!=0)
-    {
-        count++;
-        num=num%10;
-    }
-    return count;
-}
+struct node{
+    int digit;
+    struct node *next;
+} *start=NULL;
 
-int* getIntegerPart(double num){
-    int i;
-    int a[NumberOfDigits(round(num))];
-    double rem;
-    for(i=0;i<NumberOfDigits(round(num));i++)
-    {
-        rem=(int)(num) % 10;
-        num=num/10;
-        a[NumberOfDigits(round(num))-i]=(int)rem;
-    }
-    return a; 
-}
-int* getFractionalPart(double num){
-    int frac = num -(int)num;
-    int b[3];
-    int i=0;
-    double num1;
-    while(num!=(int)num)
-    {
-        num1=num*10;
-        b[i]=round(num);
-        i++;
-    }
-    return b;
-    
-    
-}
+struct node* addstart();
+struct node* addend();
+void display();
 
 int main()
 {
-    float num;
-    int i=0;
-    int count=0;
-    scanf("%f",&num);
-    
-    int IntegerPart=(int)num;
-    
-    float FractionalPart=num-IntegerPart;
-    int loc=DecimalPlaces(num);
-    int *left,*right;
-    left=getFractionalPart(num);
-    right=getIntegerPart(num);
-    
-  // printf("In the given fractional number,the integer part is %d and the fractional part is %.3f \n\n The decimal is present at location %d ",IntegerPart,FractionalPart,loc);
-   printf("The digits separated out:\n");
-    for(i=0;i<sizeof(getIntegerPart(num))/sizeof(int) - 1;i++){
-   printf("%d\t",*(left+sizeof(int)*i));
-   }
-   for(i=0;i<sizeof(getFractionalPart(num))/sizeof(int) - 1;i++){
-   printf("%d\t",*(right+sizeof(int)*i));
-   }
-  
-    
+    int intpart,dig,count;
+    float given,x,decpart;
+    scanf("%f",&given);
+    intpart=(int) given;
+    decpart=given-intpart;
+    count=0;
+    while(1)
+    {
+        dig=intpart%10;
+        if(dig>0)
+            start=addstart(dig);
+        else
+            start=addstart(-1*dig);
+        count++;
+        intpart=intpart/10;
+        if(intpart==0)
+            break;
+    }
+    while(1)
+    {
+        given=given*10;
+        dig=((int)given)%10;
+        if(dig>0)
+            start=addend(dig);
+        else
+            start=addend(-1*dig);
+        if(given-(int)given == 0)
+            break;
+    }
+    display();
+    printf("Point is %d from the beginning\n",count);
+
+}
+struct node* addstart(int k)
+{
+    struct node *temp;
+    temp=(struct node*)malloc(sizeof(struct node));
+    temp->digit=k;
+    temp->next=start;
+    start=temp;
+    return start;
+}
+struct node* addend(int k)
+{
+    struct node *ptr,*temp;
+    temp=(struct node*)malloc(sizeof(struct node));
+    temp->digit=k;
+    temp->next=NULL;
+    ptr=start;
+    if(ptr==NULL)
+    {
+        start=temp;
+        return start;
+    }
+    else
+    {
+        while(ptr->next!=NULL)
+        {
+            ptr=ptr->next;
+        }
+        ptr->next=temp;
+        return start;
+    }
+}
+void display()
+{
+    struct node *ptr;
+    ptr=start;
+    while(ptr->next!=NULL)
+    {
+        printf("%d ",ptr->digit);
+        ptr=ptr->next;
+    }
+    printf("%d\n",ptr->digit);
+    return;
 }
 
